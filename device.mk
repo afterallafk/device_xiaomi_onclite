02998e4b94c8d0ea95fb8552efb965218ae632a6
anggialansori404 <anggialansori.rpl4@gmail.com>
2021-09-22 19:32:12 +0200

    onclite: Add Pixel 2016 exclusive xml Signed-off-by: anggialansori404 <anggialansori.rpl4@gmail.com>

--- device.mk
+++ device.mk
@@ -196,6 +196,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libwifi-hal-ctrl
 
+# Google Photos
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # Health HAL
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \

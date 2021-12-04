eaecb118ba50b2adbdf018c9d01f69ca495cae2c
afterallafk <shivamatiet2001@gmail.com>
2021-12-04 23:39:43 +0530

    Pixel Experience Bringup

--- aosp_onclite.mk
+++ aosp_onclite.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
 # Inherit some common AEX stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_onclite

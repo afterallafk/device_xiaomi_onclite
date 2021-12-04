fb00561f6d7f07c6125ff4e5b956276c195db1f0
afterallafk <shivamatiet2001@gmail.com>
2021-12-05 00:29:57 +0530

    onclite: Set Boot Animation Resolution

--- aosp_onclite.mk
+++ aosp_onclite.mk
@@ -12,8 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from onclite device
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
-# Inherit some common AEX stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 720
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_onclite

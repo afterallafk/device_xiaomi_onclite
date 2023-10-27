5378ce0c3b30d8618935885fdce257b001e31691
afterallafk <shivamatiet2001@gmail.com>
2023-10-27 12:15:43 +0530

    onclite: Update media HAL branch
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,6 +6,6 @@ rm -rf hardware/qcom-caf/msm8996/media
 rm -rf hardware/qcom-caf/msm8996/display
 rm -rf hardware/qcom-caf/wlan
 git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_audio.git -b 13 hardware/qcom-caf/msm8996/audio
-git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_media.git -b 12 hardware/qcom-caf/msm8996/media
+git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_media.git -b 13 hardware/qcom-caf/msm8996/media
 git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_display.git -b 13 hardware/qcom-caf/msm8996/display
 git clone https://github.com/afterallafk/hardware_qcom-caf_wlan.git -b 12 hardware/qcom-caf/wlan

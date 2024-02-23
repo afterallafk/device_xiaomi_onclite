f864ac3d496765702a6eba517035584440264152
afterallafk <shivamatiet2001@gmail.com>
2024-02-23 22:59:44 +0530

    onclite: export CCACHE and update vendorsetup
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,8 @@
+export USE_CCACHE=1
+export CCACHE_COMPRESS=1
+export CCACHE_DIR=~/ccache
+export CCACHE_MAXSIZE=50G
+
 # Remove existing HALs
 rm -rf hardware/qcom-caf/msm8996/audio
 rm -rf hardware/qcom-caf/msm8996/media
@@ -8,6 +13,10 @@ git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_audio.git -b 
 git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_media.git -b 14 hardware/qcom-caf/msm8996/media
 git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_display.git -b 14 hardware/qcom-caf/msm8996/display
 
+# clone hardware/qcom-caf/common
+rm -rf hardware/qcom-caf/common
+git clone https://github.com/afterallafk/hardware_qcom-caf_common.git -b lineage-21.0 hardware/qcom-caf/common
+
 # Clone Proton-clang
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton
 
@@ -15,4 +24,4 @@ git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebui
 git clone --depth=1 https://github.com/afterallafk/kernel_xiaomi_onclite-dynamic -b dynamic kernel/xiaomi/onclite
 
 # Clone Vendor
-git clone https://github.com/afterallafk/android_vendor_xiaomi_onclite -b 13 vendor/xiaomi/onclite
+git clone --depth=1 https://github.com/afterallafk/android_vendor_xiaomi_onclite -b 13 vendor/xiaomi/onclite
